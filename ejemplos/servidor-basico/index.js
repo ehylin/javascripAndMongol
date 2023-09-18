d32d43bfec38ccff72b30331b7f2be1f97831bdb
Ehylin <ehylinolezjua@gmail.com>
2023-09-18 20:06:18 +0200

    add servidor

--- ejemplos/servidor-basico/index.js
+++ ejemplos/servidor-basico/index.js
@@ -2,8 +2,13 @@
 const http = require('http')
 
 //definir un servidor
-const servidor = http.createServer()
+const servidor = http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-type': 'text/plain' })
+
+    response.end(`Mape up, Neo`)
+})
 
 //arrancamos el servidor
+servidor.listen(1337, '127.0.0.1')
 
-
+console.log('Servidor arrancado http://127.0.0.1:1337')
